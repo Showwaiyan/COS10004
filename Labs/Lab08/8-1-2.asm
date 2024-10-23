38661ea8bb47cb449186de2aa677cd69918d1b9d
Show Wai Yan <showwaiyan555@gmail.com>
2024-10-23 13:45:36 +0800

    Added 8.1.1 to 8.1.2 for code reuse

--- Labs/Lab08/8-1-2.asm
+++ Labs/Lab08/8-1-2.asm
@@ -1,9 +1,18 @@
 // Variable assignment
+mov r0, 15
 mov r1, #remain
 mov r2, #blank
 mov r3, #askNumber
 
 // Procedure
+// Display totoal value
+ldr r0, .InputNum
+str r0, .WriteUnsignedNum
+str r1, .WriteString
+str r2, .WriteString
+
+// Dispaly how many want to remove
+str r3, .WriteString
 ldr r4, .InputNum
 str r4, .WriteUnsignedNum
 str r1, .WriteString
