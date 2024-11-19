220b69e5ca477d9433d5cbec3b90e496e850562d
Show Wai Yan <showwaiyan555@gmail.com>
2024-11-19 11:59:15 +0800

    Stage 1 Finished

---
+++ Assignment2/stage_1.asm
@@ -0,0 +1,111 @@
+// Main Procedure
+start:
+    bl _initialize_game
+halt
+
+
+_initialize_game: // Initialize the game
+    push {lr}
+    bl _ask_player_name
+    bl _ask_matchsticks_number
+
+    // Print the player name
+    bl _print_player_name 
+
+    // Print the number of matchsticks
+    bl _print_matchsticks_number
+
+    pop {lr}
+    ret
+
+_ask_player_name: // Ask the player for their name
+    // Print the prompt
+    mov r0, #inputNamePrompt
+    str r0, .WriteString
+
+    // Get the player name
+    mov r0, #playerName
+    str r0, .ReadString
+    str r0, .WriteString
+    push {lr}
+    bl _print_newline
+    pop {lr}
+    ret
+
+_print_player_name: // Print the player name
+    mov r0, #outputNamePrompt
+    str r0, .WriteString
+    mov r1, #playerName
+    str r1, .WriteString
+    mov r0, #newline
+    str r0, .WriteString
+    ret
+
+_ask_matchsticks_number: // Ask the player for the number of matchsticks
+    // Print the prompt
+    mov r0, #inputMatchsticksPrompt
+    str r0, .WriteString
+
+    // Get the number of matchsticks
+    ldr r0, .InputNum
+    push {lr, r2, r3}
+    mov r2, #10
+    mov r3, #100
+    bl _is_valid_matchsticks_number
+    pop {lr, r2, r3}
+    str r0, .WriteUnsignedNum
+    str r0, matchSticks
+    push {lr}
+    bl _print_newline
+    pop {lr}
+    ret
+
+_print_matchsticks_number: // Print the number of matchsticks
+    mov r0, #outputMatchsticksPrompt
+    str r0, .WriteString
+    ldr r0, matchSticks
+    str r0, .WriteUnsignedNum
+    push {lr}
+    bl _print_newline
+    pop {lr}
+    ret
+
+_is_valid_matchsticks_number: // Check if the number of matchsticks is valid
+    cmp r0, r2
+    blt _invalid_matchstick_input
+    cmp r0, r3
+    bgt _invalid_matchstick_input
+    ret
+
+_invalid_matchstick_input: // Print an error message for invalid matchsticks input
+    mov r1, #invalidInputMessage
+    str r1, .WriteString
+    push {lr}
+    bl _print_newline
+    pop {lr}
+    pop {lr, r2, r3}
+    b _ask_matchsticks_number
+
+_print_newline:
+    push {r0}
+    mov r0, #newline
+    str r0, .WriteString
+    pop {r0}
+    ret
+
+.align 256
+
+newline: .asciz "\n"
+
+// label for the name
+inputNamePrompt: .asciz "Please enter your name: "
+outputNamePrompt: .asciz "Player 1 is "
+playerName: .block 128
+
+// label for matchsticks
+inputMatchsticksPrompt: .asciz "How many matchsticks (10-100)?\n"
+outputMatchsticksPrompt: .asciz "Matchsticks: "
+matchSticks: .word 0
+
+// label for erro message
+invalidInputMessage: .asciz "Invalid input. Please enter a valid range number.\n"
